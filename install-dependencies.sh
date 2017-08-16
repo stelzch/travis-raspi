477e823cafa8dfe5adc90a8f379d96ec738859a8
Christoph Stelz <mail@ch-st.de>
2017-08-16 23:23:40 +0200

    travis: install with sudo

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,16 +5,16 @@ wget https://downloads.raspberrypi.org/raspbian_lite/root.tar.xz \
     -O $OSDIR.tar.xz
 mkdir $OSDIR
 tar xJf $OSDIR.tar.xz -C $OSDIR
-sudo cp /usr/bin/qemu-arm-static $OSDIR/usr/bin
+cp /usr/bin/qemu-arm-static $OSDIR/usr/bin
 echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin" >> $OSDIR/root/.bashrc
 echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' > /proc/sys/fs/binfmt_misc/register
-sudo mount --bind /sys $OSDIR/sys
-sudo mount --bind /dev $OSDIR/dev
-sudo mount --bind /dev/pts $OSDIR/dev/pts
-sudo mount --bind /proc $OSDIR/proc
-sudo chroot $OSDIR /usr/bin/apt update
-sudo umount $OSDIR/dev/pts
-sudo umount $OSDIR/sys
-sudo umount $OSDIR/proc
-sudo umount $OSDIR/dev
+mount --bind /sys $OSDIR/sys
+mount --bind /dev $OSDIR/dev
+mount --bind /dev/pts $OSDIR/dev/pts
+mount --bind /proc $OSDIR/proc
+chroot $OSDIR /usr/bin/apt update
+umount $OSDIR/dev/pts
+umount $OSDIR/sys
+umount $OSDIR/proc
+umount $OSDIR/dev
 
